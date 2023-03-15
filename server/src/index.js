a1387332790fb61f02ff95631bb2d3e6b983a87d
lik3a <lik3games@gmail.com>
2023-03-15 17:18:30 -0400

    Trying to insert BoxDrone and DronePart link

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import user_mdw from './routes/user.js'
 import product_mdw from './routes/product.js'
+import links_mdw from './routes/links.js'
 const app = express();
 
 app.set('port', process.env.PORT || 8080);
@@ -11,5 +12,6 @@ app.listen(app.get('port'), () => {
 app.use(express.json()); //Middleware that parses into json  
 app.use(user_mdw); //Route middlewares 
 app.use(product_mdw);
+app.use(links_mdw);
 
 export default app;
